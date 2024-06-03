ff1f971e3c7d7921a9a78586b5214123bfb56a61
kevinleeSHU <2548836183@qq.com>
2024-06-03 22:35:31 +0800

    cmake output

---
+++ 09_cmake_output/test_xlog/test_xlog.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "xlog.h"
+
+int main() {
+    XLog xlog;
+    std::cout << "test xlog" << std::endl;
+
+    return 0;
+}
---
+++ 09_cmake_output/xlog/xlog.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef _WIN32
+    #define XCPP_API 
+#else  
+    #ifdef xlog_STATIC
+        #define XCPP_API
+    #else
+        #ifdef xlog_EXPORTS
+            #define XCPP_API __declspec(dllexport)
+        #else 
+            #define XCPP_API __declspec(dllimport)
+        #endif
+    #endif
+#endif
+
+class XCPP_API XLog {
+public:
+    XLog();
+};
+
